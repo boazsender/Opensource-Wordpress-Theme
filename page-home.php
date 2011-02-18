72c9b1b6e894521028ac924ab2cf5a157d852f35
Boaz Sender <boaz@Boazs-Desktop-500.local>
2011-02-18 15:53:20 -0500

    workin on demo

--- page-home.php
+++ page-home.php
@@ -23,35 +23,29 @@ get_header(); ?>
           <div class="entry-content panel">
             <script src="http://popcornjs.org/butter/popcorn-js/popcorn.js"></script>
             <script src="http://popcornjs.org/butter/popcorn-js/plugins-playback/googlemap/popcorn.googlemap.js"></script>
+            <script src="http://popcornjs.org/butter/popcorn-js/plugins-playback/twitter/popcorn.twitter.js"></script>
 
             <video id="video" src="http://webmademovies.etherworks.ca/popcorndemo/demoscreencast.ogv" controls=""></video>
 
             <div class="sidepanel">
             <div id="map-container" data-plugin="googlemap" class="butter-plugin"></div>
+            <div id="twitter-container" data-plugin="twitter" class="butter-plugin"></div>
 
             <script>
               $(function(){
-                $('#runDemo').click(function(){
-                  eval($('#thecode').text());
+                $(function () { 
+                 var $p = Popcorn("#video")
+                   .googlemap({"id":"googlemap","start":1,"end":8,"target":"map-container","type":"TERRAIN","zoom":12,"location":"Boston"})
+                   .googlemap({"id":"googlemap","start":8,"end":16,"target":"map-container","type":"TERRAIN","zoom":12,"location":"Toronto"})
+                   .googlemap({"id":"googlemap","start":16,"end":90,"target":"map-container","type":"TERRAIN","zoom":12,"location":"San francisco"})
+                   .twitter({"id":"twitter","start":0,"end":8,"target":"twitter-container","src":"@popcornjs"})
+                   .twitter({"id":"twitter","start":8,"end":16,"target":"twitter-container","src":"@mozilla"})
+                   .twitter({"id":"twitter","start":16,"end":24,"target":"twitter-container","src":"@bocoup"})
+                   .twitter({"id":"twitter","start":24,"end":90,"target":"twitter-container","src":"@popcornjs"});
                 });
               });
             </script>
 
-            <pre class="demo">
-&lt;script src="https://github.com/webmademovies/popcorn-js/raw/master/popcorn.js"&gt;&lt;/script&gt;
-&lt;script&gt;
-<code id="thecode" contenteditable="">
-$(function () { 
- var $p = Popcorn("#video")
-   .googlemap({"id":"googlemap","start":1,"end":8,"target":"map-container","type":"TERRAIN","zoom":12,"location":"Boston"})
-   .googlemap({"id":"googlemap","start":8,"end":16,"target":"map-container","type":"TERRAIN","zoom":12,"location":"Toronto"})
-   .googlemap({"id":"googlemap","start":16,"end":90,"target":"map-container","type":"TERRAIN","zoom":12,"location":"San francisco"})
-   .play();
-});
-</code>
-&lt;/script&gt;
-            </pre>
-            <h3 id="runDemo">&uarr; Click to run this code</h3>
 
             </div>
           </div><!-- .entry-content -->
