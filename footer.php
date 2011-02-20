bce2afb4c5418f247a34bbc633a26af70fe2fbaf
Boaz Sender <boazsender@Boaz-Senders-MacBook.local>
2011-02-20 18:55:52 -0500

    updated footer

--- footer.php
+++ footer.php
@@ -40,14 +40,11 @@
       </div>
       <div class="third last pushtoright">
         <p>
-          Web Made Movies is a <a href="http://mozilla.com">Mozilla</a> funded project. This website is hosted by Media Temple.
+          Web Made Movies is a <a href="http://mozilla.com">Mozilla</a> funded project. This website is hosted by Media Temple. See a full list of the project sponsors <a href="/sponsors">here</a>.
         </p>
         <footer>
-          <a href="http://www.w3.org/html/logo/">
-            <img src="http://www.w3.org/html/logo/downloads/HTML5_Badge_64.png" height="48" alt="HTML5 Powered with CSS3 / Styling, Multimedia, Performance &amp; Integration, Semantics, and Offline &amp; Storage" title="HTML5 Powered with CSS3 / Styling, Multimedia, Performance &amp; Integration, Semantics, and Offline &amp; Storage">
-          </a>
           <a href="http://mozilla.com" title="The Mozilla Foundation">
-            <img height="50px"src="<?php echo bloginfo( 'template_directory' ) ?>/img/mozilla.png"> 
+            <img class="right" height="30px"src="<?php echo bloginfo( 'template_directory' ) ?>/img/mozilla.png"> 
           </a>
         </footer>
       </div>
