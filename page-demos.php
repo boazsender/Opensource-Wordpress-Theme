6db5ba1a54571f8fe2458e9b76ce6b16c06d9ac4
Boaz Sender <boaz@Boazs-Desktop-500.local>
2011-02-21 13:39:40 -0500

    added line breaker to demo page

--- page-demos.php
+++ page-demos.php
@@ -18,9 +18,16 @@ get_header(); ?>
       <div id="content" role="main">
 
 
-<?php $loop = new WP_Query( array( 'post_type' => 'demo', 'posts_per_page' => 99999 ) ); ?>
+<?php
+$counter = 0;
+$loop = new WP_Query( array( 'post_type' => 'demo', 'posts_per_page' => 99999 ) ); 
+?>
 
-<?php while ( $loop->have_posts() ) : $loop->the_post(); ?>
+<?php
+  while ( $loop->have_posts() ) : $loop->the_post(); 
+  $counter = $counter + 1;
+
+?>
 
         <div id="post-<?php the_ID(); ?>" <?php post_class('box'); ?>>
           <h1 class="entry-title"><?php the_title(); ?></h1>
@@ -31,8 +38,12 @@ get_header(); ?>
             <?php edit_post_link( __( 'Edit', 'twentyten' ), '<span class="edit-link">', '</span>' ); ?>
           </div><!-- .entry-content -->
         </div><!-- #post-## -->
-
-        <?php #comments_template( '', true ); ?>
+        
+<?php 
+  if ( $counter % 4 == 0 ) {
+    echo '<hr class="space">';
+  }
+?>
 
 <?php endwhile; ?>
 
