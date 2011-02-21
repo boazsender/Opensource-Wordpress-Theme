4ab02ebe95e0196518bbf0db0d48be7e01dfd941
Boaz Sender <boazsender@Boaz-Senders-MacBook.local>
2011-02-20 19:14:10 -0500

    fixed blog post page

--- single.php
+++ single.php
@@ -28,9 +28,31 @@ if ( get_post_type() == 'Demo' ){
         <div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
           <div class="entry-content">
-            <h1>.<?php the_title(); if ( $terms[0]->slug != 'instance-properties' ) { echo '()';};?></h1>
+            <h1 class="fancy"><?php the_title();?></h1>
             <?php the_content(); ?>
             <?php wp_link_pages( array( 'before' => '<div class="page-link">' . __( 'Pages:', 'twentyten' ), 'after' => '</div>' ) ); ?>
+
+            <?php if ( get_the_author_meta( 'description' ) ) : // If a user has filled out their description, show a bio on their entries  ?>
+              <div id="entry-author-info">
+                <div id="author-avatar">
+                  <?php echo get_avatar( get_the_author_meta( 'user_email' ), apply_filters( 'twentyten_author_bio_avatar_size', 60 ) ); ?>
+                </div><!-- #author-avatar -->
+                <div id="author-description">
+                  <h2><?php printf( esc_attr__( 'About %s', 'twentyten' ), get_the_author() ); ?></h2>
+                  <?php the_author_meta( 'description' ); ?>
+                  <div id="author-link">
+                    <a href="<?php echo get_author_posts_url( get_the_author_meta( 'ID' ) ); ?>">
+                      <?php printf( __( 'View all posts by %s <span class="meta-nav">&rarr;</span>', 'twentyten' ), get_the_author() ); ?>
+                    </a>
+                  </div><!-- #author-link	-->
+                </div><!-- #author-description -->
+              </div><!-- #entry-author-info -->
+            <?php endif; ?>
+
+            <div class="fine">
+              <?php twentyten_posted_on(); ?>
+            </div>
+            
           </div>
         </div>
 
