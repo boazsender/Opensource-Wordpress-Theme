0e3f39d4b93b67e4ac19f12f8e58a7c17a3d01ee
Boaz Sender <boazsender@Boaz-Senders-MacBook.local>
2011-02-20 19:02:41 -0500

    added fancy header to pages

--- page-onecolumn.php
+++ page-onecolumn.php
@@ -20,7 +20,7 @@ get_header(); ?>
 <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
 
         <div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-          <h1 class="entry-title"><?php the_title(); ?></h1>
+          <h1 class="fancy large"><?php the_title(); ?></h1>
           <div class="entry-content">
             <?php the_content(); ?>
             <?php wp_link_pages( array( 'before' => '<div class="page-link">' . __( 'Pages:', 'twentyten' ), 'after' => '</div>' ) ); ?>
